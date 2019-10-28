e3f0c193d4f56ff9bc2c75195af46a5d4c320545
Adryel <43222687+adryel97@users.noreply.github.com>
2019-10-28 13:03:24 -0300

    Inserindo código

--- aula04/index.php
+++ aula04/index.php
@@ -1,1 +1,23 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Aula 04 - PHP POO</title>
+</head>
+<body>
+    <pre>
+    <?php
+        require_once 'Caneta.php';
+        $c1 = new Caneta("Bic", "Azul", "0.5");
+        $c2 = new Caneta("JIST", "Vermelha", "1.5");
+        
+        print_r($c1);
+        print_r($c2);
+        //$c1->modelo = "Ric"; ERRO porque o sómente usando o método set para acessar
+        //$c1->ponta = 0.4; 
+    ?>
+    </pre>
+</body>
+</html>
