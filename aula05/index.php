119c6e01d5b4a2f5dd3756321c8bd71acb8ffaad
Adryel <43222687+adryel97@users.noreply.github.com>
2019-10-28 16:59:36 -0300

    Inserindo código

--- aula05/index.php
+++ aula05/index.php
@@ -1,1 +1,44 @@
 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Aula03 - POO PHP</title>
+</head>
+<body>
+    <pre>
+        <?php
+            require_once'ContaBanco.php';
+
+            $p1 = new ContaBanco(); //Creusa
+            $p2 = new ContaBanco();//Jubileu
+            
+            
+            $p1->abrirConta("CP");
+            $p1->setDono("Creusa");
+            $p1->setNumConta(11111);
+
+            $p2->abrirConta("CC");
+            $p2->setDono("Jubileu");
+            $p2->setNumConta(22222);
+
+            $p1->sacar(1000);
+
+            $p1->depositar(500);
+            $p2->depositar(300);
+
+            $p1->pagarMensal();
+            $p2->pagarMensal();
+
+            $p1->sacar(630);
+            $p2->sacar(338);
+
+            $p1->fecharConta();
+            $p2->fecharConta();
+            print_r($p1);
+            print_r($p2);
+        ?>
+    </pre>
+</body>
+</html>
