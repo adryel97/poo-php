f9a89a941f3a2d29c20ab9ddbffb4edcc133787f
Adryel <43222687+adryel97@users.noreply.github.com>
2019-10-28 13:04:19 -0300

    Inserindo código

--- aula04/Caneta.php
+++ aula04/Caneta.php
@@ -1,1 +1,36 @@
 
+<?php
+
+class caneta{
+    private $modelo;
+    private $ponta;
+    private $tampada;
+    private $cor;
+
+
+    public function Caneta($m, $c, $p){
+        $this->modelo = $m;
+        $this->cor = $c;
+        $this->ponta = $p;
+        $this->tampar();
+    }
+    /*public function __construct(){
+        $this->cor = "Azul"; //constroi ja a caneta
+        $this->tampar(); 
+    }*/
+    public function tampar(){
+        $this->tampada = true;
+    }
+    public function getModelo(){
+        return $this->modelo;
+    }
+    public function setModelo($m){
+        $this->modelo = $m;
+    }
+    public function getPonta(){
+        return $this->ponta;
+    }
+    public function setPonta($p){
+        $this->ponta = $p;
+    }
+}
