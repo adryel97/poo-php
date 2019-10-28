b20b7f996b6d9713eafd65f4c762197d9997d203
Adryel <43222687+adryel97@users.noreply.github.com>
2019-10-28 11:59:38 -0300

    Criado o código

--- aula02/Caneta.php
+++ aula02/Caneta.php
@@ -1,1 +1,24 @@
 
+<?php
+    class Caneta{
+        var $modelo;
+        var $cor;
+        var $ponta;
+        var $carga;
+        var $tampada;
+        
+        function rabiscar(){
+            if($this->tampada == true){
+                echo "<p>ERRO! Não posso rabiscar</p>"
+            } else {
+                echo "<p>Rabiscando</p>"
+            }
+        }
+        function tampar(){
+            $this->tampada = true;
+        }
+        function destampar(){
+            $this->tampada = false;
+        }
+    }
+    
